968319bb00ce1e9d8fd77b8c206293d9ab50e298
Piotr Komar <piotr_komar@wp.pl>
2016-04-03 12:53:57 +0200

    refined and fixed parser -> added printing of 'nam' value

--- python/data_parser.py
+++ python/data_parser.py
@@ -1,13 +1,27 @@
 import xml.etree.ElementTree
 
 e = xml.etree.ElementTree.parse('../data/kpwr-1.2.7-names-disamb-nam-flatten/techniczne/00101534.xml').getroot()
+# e = xml.etree.ElementTree.parse('../data/kpwr-1.2.7-names-disamb-nam-flatten/stenogramy/00101522.xml').getroot()
+
+printType = True
 
 for chunk in e.findall('chunk'):
     for sentence in chunk.findall('sentence'):
-        text = ""
+        properName = ""
+        nameId = '0'
         for tok in sentence.findall('tok'):
             for ann in tok.findall('ann'):
+                if ann.attrib['chan'] == 'nam' and ann.text != nameId and properName != "":
+                    if printType:
+                        properName += " " + nameId
+                    print properName
+                    properName = ""
                 if ann.attrib['chan'] == 'nam' and ann.text != '0':
-                    text = text + tok.find('orth').text + " "
-        if text != "":
-            print text + "\n"
+                    nameId = ann.text
+                    if properName != "":
+                        properName += " "
+                    properName += tok.find('orth').text
+        if properName != "":
+            if printType:
+                properName += " " + nameId
+            print properName
